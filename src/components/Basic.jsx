1e7d2f374a89bc376788f5e5fa380b7c7da0f6fa
Natiz Bista <bistanatiz10@gmail.com>
2023-01-01 18:39:33 +0545

    initial commit

---
+++ src/components/Basic.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useFormik } from "formik";
+
+function Basic() {
+  return (
+    <div>
+      <form>
+        <div>
+          <label className="bg-red-400 text-lg">Name</label>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Basic;
