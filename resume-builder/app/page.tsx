ba5d6c09a5d33497e11fb7d0b93e691b3c23b2b0
Natiz Bista <bistanatiz10@gmail.com>
2023-07-03 19:48:09 +0545

    redux setup

--- resume-builder/app/page.tsx
+++ resume-builder/app/page.tsx
@@ -1,12 +1,15 @@
-'use client'
 import Image from 'next/image';
 import Button from '@mui/material/Button';
 import HomePage from '../components/HomePage';
 
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="w-full h-full">
-      <HomePage />    
+      <HomePage />   
+      <Link href = "/events">Event</Link>
+      <button>Events</button> 
       </main>
   )
 }
