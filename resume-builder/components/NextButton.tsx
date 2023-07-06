7fef61d66a887ebc51c7a9301c68b82604c97403
Natiz Bista <bistanatiz10@gmail.com>
2023-07-06 21:34:07 +0545

    handled steps and active step

--- resume-builder/components/NextButton.tsx
+++ resume-builder/components/NextButton.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Button} from '@mui/material'
+import { useDispatch } from 'react-redux';
+import { addStep,subtractStep } from '@/redux/reducers/StepReducer';
 
 const NextButton = () => {
+  const dispatch = useDispatch()
   return (
     <div className='w-full flex items-center justify-around p-4'>
-        <Button variant='outlined'>Prev</Button>
-        <Button variant='outlined'>Next</Button>
+        <Button variant='outlined' onClick={()=>dispatch(subtractStep({}))}>Prev</Button>
+        <Button variant='outlined' onClick={()=>dispatch(addStep({}))}>Next</Button>
     </div>
   )
 }
--- resume-builder/components/Steps.tsx
+++ resume-builder/components/Steps.tsx
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
 
 const steps = [
   'Personal Info',
@@ -12,10 +14,11 @@ const steps = [
 ];
 
 export default function Steps() {
+  const step = useSelector((state:RootState) => state.step.step)
   return (
     <div className='w-full'>
     <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={0} alternativeLabel>
+      <Stepper activeStep={step} alternativeLabel>
         {steps.map((label) => (
           <Step key={label}>
             <StepLabel>{label}</StepLabel>
